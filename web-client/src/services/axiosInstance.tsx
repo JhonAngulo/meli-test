c683e0cb01244b0021880c53a5325511f943e863
Jhon Angulo <jhon.angulo@jikkosoft.com>
2023-03-19 19:07:45 -0500

    implement husky

--- web-client/src/services/axiosInstance.tsx
+++ web-client/src/services/axiosInstance.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-console.log('env', import.meta.env.VITE_PUBLIC_SERVER_URL_BASE)
 const apiInstance = axios.create({
   baseURL: import.meta.env.VITE_PUBLIC_SERVER_URL_BASE,
   timeout: 5000
